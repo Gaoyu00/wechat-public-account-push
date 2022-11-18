b97a5e2e371cbe54de690bb6845eb72fb11fbd91
Gaoyu00 <118499551+Gaoyu00@users.noreply.github.com>
2022-11-18 12:35:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oAwlw5wwCQlQ66BBQfG7d3FErhzI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'ID3giCTeQXCBe9Qz8ICgtH0YTdzqV4-cg5ing_HT-zc,
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oAwlw5wwCQlQ66BBQfG7d3FErhzI',
+      id: 'oAwlw5xixWTlA4zGsxWJXN4U8sBA',
     }
   ],
 
