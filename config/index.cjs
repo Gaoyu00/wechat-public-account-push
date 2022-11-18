08772c80df61276bc7fc80844dfc9c4bbc0c7a32
Gaoyu00 <118499551+Gaoyu00@users.noreply.github.com>
2022-11-18 13:11:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'bb',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oAwlw5wwCQlQ66BBQfG7d3FErhzI',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ID3giCTeQXCBe9Qz8ICgtH0YTdzqV4-cg5ing_HT-zc,
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
